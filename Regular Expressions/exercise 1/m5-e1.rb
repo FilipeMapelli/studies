3708a7043ee5741a547cfa7893ba17b9a6a4fe27
Filipe Mapelli <filipemapellis@gmail.com>
2016-08-05 21:55:14 -0300

    Exercise #1 - class declaration

---
+++ Regular Expressions/exercise 1/m5-e1.rb
@@ -0,0 +1,16 @@
+class M5E1
+    
+    NOTICIAS =  "09:55 - [poltica] Prefeito da Krakosia  eleito o melhor para economia\n" + 
+                "10:14 - [cotidiano] Cachorro salva dono de assalto\n" +
+                "11:01 - [esporte] Seleo local de golfe se classifica para mundial\n" +
+                "11:36 - [esporte] Joo da Silva Sauro  cortado da seleo de golfe\n" +
+                "13:19 - [economia] Desemprego sobe para 12% no ltimo ms\n " +
+                "14:55 - [poltica] Lei de incentivo ao esporte  aprovada no senado s 10:20\n" +
+                "16:13 - [economia] Cotao do dlar sobe 5% em uma semana\n" +
+                "19:26 - [esporte] Liga nacional de basquete tem recorde de pblico"
+                
+    def format_news(string)
+        
+    end
+    
+end
