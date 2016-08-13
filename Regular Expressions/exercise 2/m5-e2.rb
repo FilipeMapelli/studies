314a6b6ae53f025b9fb84c64892de6df83776e42
Filipe Mapelli <filipemapellis@gmail.com>
2016-08-12 23:42:53 -0300

    added price_validation method

--- Regular Expressions/exercise 2/m5-e2.rb
+++ Regular Expressions/exercise 2/m5-e2.rb
@@ -12,9 +12,18 @@ class M5E2
    #validar se o valor do parâmetro recebido é um email
    def email_validation(email)
        if email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/)
-          return true 
+          true 
        else
-          return false 
+          false 
+       end
+   end
+
+   #validar se o parâmetro recebido é um preço em real ou em dólar
+   def price_validation(price)
+       if price.match(/\AR\$(\s|)(\d{1,3}(\.\d{3})*)(\,\d{2})\z|\AU\$(\s|)(\d{1,3}(\,\d{3})*)(\.\d{2})\z/)
+          true 
+       else
+          false
        end
    end
    
