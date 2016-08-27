0d55d09414f903044e65317f12143716dea97c4f
Filipe Mapelli <filipemapellis@gmail.com>
2016-08-26 21:32:10 -0300

    Shell script - creating of files

---
+++ Unix - Shell script/e1/m1-e1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/workspace/ep-exercicios/m1/e1/dir_1
+
+mkdir -p ~/workspace/ep-exercicios/m1/e1/dir_1/treinamento_estagio_programacao
+
+ln -s ~/workspace/ep-exercicios/m1/e1/dir_1/treinamento_estagio_programacao ~/workspace/ep-exercicios/m1/e1/dir_1/TEP   
+
+mkdir -p ~/workspace/ep-exercicios/m1/e1/dir_1/treinamento_estagio_programacao/aluno_fernando
+
+ln -s ~/workspace/ep-exercicios/m1/e1/dir_1/treinamento_estagio_programacao/aluno_fernando ~/workspace/ep-exercicios/m1/e1/dir_1/treinamento_estagio_programacao/fer
+
+touch ~/workspace/ep-exercicios/m1/e1/dir_1/treinamento_estagio_programacao/aluno_fernando/notas.txt
