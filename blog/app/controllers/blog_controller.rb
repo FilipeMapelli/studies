70d127932b2211f7143872c76acb21a1fd93737e
Filipe Mapelli <filipemapellis@gmail.com>
2016-10-10 15:11:18 -0300

    Created article page

--- blog/app/controllers/blog_controller.rb
+++ blog/app/controllers/blog_controller.rb
@@ -1,5 +1,9 @@
 class BlogController < ApplicationController
   def index
-    @articles= Article.all.shuffle
+    @articles = Article.all.shuffle
+  end
+  
+  def article
+  	@article = Article.find(params[:id])
   end
 end
