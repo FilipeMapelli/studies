9bd4dfc8a934861789efc7d6c9046ddf916a4fa8
Filipe Mapelli <filipemapellis@gmail.com>
2016-10-10 13:52:28 -0300

    Added validations model Article

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+	validates :category, presence: true, length: 1..20
+  validates :author, presence: true, length: 1..100
+  validates :title, presence: true, length: 3..100
+  validates :content, presence: true
 end
